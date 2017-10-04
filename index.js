d67714049ee9678be058c017df7a5eaa27c5b6a3
Akshay Menon <akshaymenon8@gmail.com>
2017-10-04 19:50:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Add to Filter
+function addToFilter(filter, sourceList, key, fnComparator) {
+    const selectedClass = sourceList.filter(fnComparator)
+    return {
+        ...filter,
+        [key]: selectedClass[0] || {},
+    }
+}
+
+// Splits key in a JSON Object
+const assignHelper = (obj, key, val) => {
+    var newObj = {...obj };
+
+
+    key.split('.').reduce((a, cv, ci, arr) => arr.length == ci + 1 ? (str) =>
+        a[cv] = str : (a[cv] = {...a[cv] }, a[cv]), newObj)(val);
+
+
+    return newObj;
+}
+
+const getValue = (obj, name) =>
+    name.split('.').reduce((a, cv) => (a || {})[cv], obj) || ''
+
+
+module.exports = {
+    addToFilter: addToFilter,
+    assign: assignHelper,
+    getValue: getValue
+}
